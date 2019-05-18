51c4c73300f48e24353fa1de15107e543c827d9d
PassiveModding <passivemodding@gmail.com>
2019-05-18 15:56:10 +1000

    Random Changes

--- RavenBOT/Modules/Lithium/Models/Setup.cs
+++ RavenBOT/Modules/Lithium/Models/Setup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RavenBOT.Modules.Lithium.Models
+﻿namespace RavenBOT.Modules.Lithium.Models
 {
     public class Setup
     {
