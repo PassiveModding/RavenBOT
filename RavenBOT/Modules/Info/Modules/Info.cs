2c5cc2cb43f1cf9362a69e05c037dc8c7f8ca6bb
PassiveModding <passivemodding@gmail.com>
2019-06-04 14:27:19 +1000

    Added command to display all members in a role

---
+++ RavenBOT/Modules/Info/Modules/Info.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace RavenBOT.Modules.Info.Modules
+{
+    [Group("info")]
+    public class Info : InteractiveBase<ShardedCommandContext>
+    {
+        [Command("RoleMembers")]
+        public async Task RoleMembersAsync(SocketRole role)
+        {
+            await Context.Guild.DownloadUsersAsync();
+            await ReplyAsync("", false, new EmbedBuilder()
+            {
+                Title = $"{role.Name} Members",
+                Description = $"{string.Join(", ", role.Members.Select(x => x.Mention))}"
+            }.Build());
+        }
+    }
+}
