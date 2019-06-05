73381ce7416c0c82bafc90515f08b1fef16dd750
PassiveModding <passivemodding@gmail.com>
2019-06-05 19:59:19 +1000

    Added string extension to handle gfycat urls

--- RavenBOT/Extensions/StringExtensions.cs
+++ RavenBOT/Extensions/StringExtensions.cs
@@ -7,6 +7,50 @@ namespace RavenBOT.Extensions
 {
     public static class StringExtensions
     {
+        public static string FixGfycatUrl(this string original)
+        {
+            if (original.Contains("gfycat", StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (original.EndsWith(".mp4", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    original = original.Replace(".mp4", ".gif", StringComparison.InvariantCultureIgnoreCase);
+                }
+                else if (original.EndsWith(".webm", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    original = original.Replace(".webm", ".gif", StringComparison.InvariantCultureIgnoreCase);
+                }
+
+                if (original.Contains("giant.", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return original;
+                }
+                else if (original.Contains("zippy.", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return original;
+                }
+                else if (original.Contains("thumbs.", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    //Fixes cdn and replaces mobile or size restricted tags.
+                    original = original.Replace("thumbs.", "zippy.", StringComparison.InvariantCultureIgnoreCase);
+                    if (original.Contains("-"))
+                    {
+                        int subStrIndex = original.IndexOf("-");
+                        original = original.Substring(0, subStrIndex) + ".gif";
+                    }
+                    return original;
+                }
+                else
+                {
+                    var startIndex = original.IndexOf("gfycat", StringComparison.InvariantCultureIgnoreCase);
+                    original = original.Substring(startIndex, original.Length - startIndex);
+                    original = $"https://zippy.{original}.gif";
+                    return original;
+                }
+            }
+
+            return original;
+        }
+
         public static string FixLength(this string value, int length = 1023)
         {
             if (value.Length > length)
