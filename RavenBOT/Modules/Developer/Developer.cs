ccd64d5f73aa826b48160f5ffbe9ec5623ac9245
PassiveModding <passivemodding@gmail.com>
2019-06-09 17:04:04 +1000

    Added method to migrate data to litedb
    this should work between any database solution but would require editing code to change

--- RavenBOT/Modules/Developer/Developer.cs
+++ RavenBOT/Modules/Developer/Developer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -7,8 +8,24 @@ using Discord.WebSocket;
 using RavenBOT.Extensions;
 using RavenBOT.Handlers;
 using RavenBOT.Models;
+using RavenBOT.Modules.AutoMod.Models;
+using RavenBOT.Modules.AutoMod.Models.Moderation;
+using RavenBOT.Modules.Birthday.Models;
+using RavenBOT.Modules.Captcha.Models;
+using RavenBOT.Modules.Conversation.Models;
+using RavenBOT.Modules.Events.Models.Events;
+using RavenBOT.Modules.Games.Models;
+using RavenBOT.Modules.Greetings.Models;
+using RavenBOT.Modules.Levels.Models;
+using RavenBOT.Modules.Moderator.Models;
+using RavenBOT.Modules.Partner.Models;
+using RavenBOT.Modules.Statistics.Models;
+using RavenBOT.Modules.Tags.Models;
+using RavenBOT.Modules.Tickets.Models;
+using RavenBOT.Modules.Translation.Models;
 using RavenBOT.Services;
 using RavenBOT.Services.Database;
+using static RavenBOT.Services.PrefixService;
 
 namespace RavenBOT.Modules.Developer
 {
@@ -140,6 +157,46 @@ namespace RavenBOT.Modules.Developer
                 await ReplyAsync(e.ToString().FixLength(2047));
             }
 
+        }
+
+        [Command("MigrateToLiteDB", RunMode = RunMode.Async)]
+        public async Task MigrateDB()
+        {
+            var newDB = new LiteDataStore();
+            newDB.StoreMany(Database.Query<LevelUser>().ToList(), x => LevelUser.DocumentName(x.UserId, x.GuildId));
+            newDB.StoreMany(Database.Query<LevelConfig>().ToList(), x => LevelConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<ActionConfig>().ToList(), x => ActionConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<ActionConfig.ActionUser>().ToList(), x => ActionConfig.ActionUser.DocumentName(x.UserId, x.GuildId));
+            newDB.StoreMany(Database.Query<BirthdayConfig>().ToList(), x => BirthdayConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<BirthdayModel>().ToList(), x => BirthdayModel.DocumentName(x.UserId));
+            newDB.Store(Database.Load<BotConfig>("BotConfig"), "BotConfig");
+            newDB.StoreMany(Database.Query<CaptchaConfig>().ToList(), x => CaptchaConfig.DocumentName(x.GuildId));
+            newDB.Store(Database.Load<ConversationConfig>(ConversationConfig.DocumentName()), ConversationConfig.DocumentName());
+            newDB.StoreMany(Database.Query<EventConfig>().ToList(), x => EventConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<GameServer>().ToList(), x => GameServer.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<GameUser>().ToList(), x => GameUser.DocumentName(x.UserId, x.GuildId));
+            newDB.StoreMany(Database.Query<GoodbyeConfig>().ToList(), x => GoodbyeConfig.DocumentName(x.GuildId));            
+            newDB.StoreMany(Database.Query<WelcomeConfig>().ToList(), x => WelcomeConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<ModerationConfig>().ToList(), x => ModerationConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<ModuleManagementService.ModuleConfig>().ToList(), x => ModuleManagementService.ModuleConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<PartnerConfig>().ToList(), x => PartnerConfig.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<Services.Licensing.LicenseService.QuantifiableLicense>().ToList(), x => x.Id);
+            newDB.StoreMany(Database.Query<Services.Licensing.LicenseService.TimedLicense>().ToList(), x => x.Id);
+            newDB.StoreMany(Database.Query<Services.Licensing.LicenseService.QuantifiableUserProfile>().ToList(), x => x.Id);
+            newDB.StoreMany(Database.Query<Services.Licensing.LicenseService.TimedUserProfile>().ToList(), x => x.Id);
+            newDB.StoreMany(Database.Query<Reminders.Models.Reminder>().ToList(), x => Reminders.Models.Reminder.DocumentName(x.UserId, x.ReminderNumber));
+            newDB.StoreMany(Database.Query<TagGuild>().ToList(), x => TagGuild.DocumentName(x.GuildId));
+            newDB.StoreMany(Database.Query<Ticket>().ToList(), x => Ticket.DocumentName(x.GuildId, x.TicketNumber));
+            newDB.StoreMany(Database.Query<TranslateGuild>().ToList(), x => TranslateGuild.DocumentName(x.GuildId));
+            newDB.Store(Database.Load<GraphiteConfig>(GrafanaConfig.DocumentName()), GrafanaConfig.DocumentName());
+            newDB.Store(Database.Load<LoggerConfig>("LogConfig"), "LogConfig");
+            newDB.Store(Database.Load<PerspectiveSetup>(PerspectiveSetup.DocumentName()), PerspectiveSetup.DocumentName());
+            newDB.Store(Database.Load<PrefixInfo>("PrefixSetup"), "PrefixSetup");
+            newDB.Store(Database.Load<DeveloperSettings.Settings>("Settings"), "Settings");
+            newDB.Store(Database.Load<TimeTracker>("TimedModerations"), "TimedModerations");
+            newDB.Store(Database.Load<TranslateConfig>("TranslateConfig"), "TranslateConfig");
+            newDB.Store(Database.Load<GraphiteConfig>(GrafanaConfig.DocumentName()), GrafanaConfig.DocumentName());            
+            await ReplyAsync("Done.");
         }
 
         public class DBTestDocument
