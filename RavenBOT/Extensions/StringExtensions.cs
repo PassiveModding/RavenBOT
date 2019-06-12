5703a11336bf8b668c817362c2fd236e9ad8cfd3
PassiveModding <passivemodding@gmail.com>
2019-06-12 17:36:33 +1000

    Enum as dictionary method added for support in elo module

--- RavenBOT/Extensions/StringExtensions.cs
+++ RavenBOT/Extensions/StringExtensions.cs
@@ -105,5 +105,16 @@ namespace RavenBOT.Extensions
                 return original;
             }            
         }
+
+        public static IDictionary<String, Int32> ConvertEnumToDictionary<K>()
+        {
+            //Ensure that the base type is actually an enum
+            if (typeof(K).BaseType != typeof(Enum))
+            {
+                throw new InvalidCastException();
+            }
+
+            return Enum.GetValues(typeof(K)).Cast<Int32>().ToDictionary(currentItem => Enum.GetName(typeof(K), currentItem));
+        }
     }
 }
