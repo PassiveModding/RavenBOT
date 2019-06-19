b25935b7c987252882a22fac235c0a4dfaed90d6
PassiveModding <passivemodding@gmail.com>
2019-06-19 17:05:43 +1000

    Added screenshare command for discord servers

---
+++ RavenBOT/RavenBOT.Modules/DiscordScreenshare.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using RavenBOT.Extensions;
+
+namespace RavenBOT.Modules
+{
+    public class DiscordScreenshare : InteractiveBase<ShardedCommandContext>
+    {
+        [Command("ScreenShare")]
+        [RequireContext(ContextType.Guild)]
+        public async Task MakeScreenshare()
+        {
+            if (!(Context.User is SocketGuildUser user))
+            {
+                return;
+            }
+
+            if (user.VoiceChannel == null)
+            {
+                await ReplyAsync("You must be in a voice channel for this to run.");
+                return;
+            }
+
+            var link = $"Click this link **while** in the voice channel and you will be placed in a video channel.\n" +
+                        $"https://discordapp.com/channels/{Context.Guild.Id}/{user.VoiceChannel.Id}\n" +
+                        $"Anyone who wishes to view the screenshare must also first join the audio channel and then click the link.";
+            await ReplyAsync("", false,  link.QuickEmbed());
+        }
+    }
+}
