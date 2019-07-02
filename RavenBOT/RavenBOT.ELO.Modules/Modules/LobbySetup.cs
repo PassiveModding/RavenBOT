30d486d55be297b9f4753cda51e6d93e948070ac
PassiveModding <passivemodding@gmail.com>
2019-07-02 11:17:05 +1000

    ELO Setup module is now lobbysetup

--- RavenBOT/RavenBOT.ELO.Modules/Modules/LobbySetup.cs
+++ RavenBOT/RavenBOT.ELO.Modules/Modules/LobbySetup.cs
@@ -10,8 +10,9 @@ using RavenBOT.Extensions;
 namespace RavenBOT.ELO.Modules.Modules
 {
     [RequireContext(ContextType.Guild)]
+    //TODO: Potential different permissions for creating lobby
     [RequireUserPermission(GuildPermission.Administrator)]
-    public class Setup : ELOBase
+    public class LobbySetup : ELOBase
     {
         [Command("Create Lobby")]
         public async Task CreateLobbyAsync(int playersPerTeam = 5, Lobby.PickMode pickMode = Lobby.PickMode.Captains)
@@ -86,51 +87,5 @@ namespace RavenBOT.ELO.Modules.Modules
             var capDict = StringExtensions.ConvertEnumToDictionary<Lobby.CaptainMode>();
             await ReplyAsync($"{string.Join("\n", capDict.Keys)}");
         }
-
-        [Command("SetRegisterRole")]
-        public async Task SetRegisterRole(IRole role)
-        {
-            var competition = Context.Service.GetCompetition(Context.Guild.Id) ?? Context.Service.CreateCompetition(Context.Guild.Id);
-            competition.RegisteredRankId = role.Id;
-            Context.Service.Database.Store(competition, CompetitionConfig.DocumentName(Context.Guild.Id));
-            await ReplyAsync("Register role set.");
-        }
-
-        [Command("AddRank")]
-        public async Task AddRank(IRole role, int points)
-        {
-            var competition = Context.Service.GetCompetition(Context.Guild.Id) ?? Context.Service.CreateCompetition(Context.Guild.Id);
-            competition.Ranks = competition.Ranks.Where(x => x.RoleId != role.Id).ToList();
-            competition.Ranks.Add(new Rank
-            {
-                RoleId = role.Id,
-                    Points = points
-            });
-            Context.Service.Database.Store(competition, CompetitionConfig.DocumentName(Context.Guild.Id));
-            await ReplyAsync("Rank added.");
-        }
-
-        [Command("AddRank")]
-        public async Task AddRank(int points, IRole role)
-        {
-            await AddRank(role, points);
-        }
-
-        [Command("RemoveRank")]
-        public async Task RemoveRank(ulong roleId)
-        {
-            var competition = Context.Service.GetCompetition(Context.Guild.Id) ?? Context.Service.CreateCompetition(Context.Guild.Id);
-            competition.Ranks = competition.Ranks.Where(x => x.RoleId != roleId).ToList();
-            Context.Service.Database.Store(competition, CompetitionConfig.DocumentName(Context.Guild.Id));
-            await ReplyAsync("Rank Removed.");
-        }
-
-        [Command("RemoveRank")]
-        public async Task RemoveRank(IRole role)
-        {
-            await RemoveRank(role.Id);
-        }
-
-        //TODO: Ranks list
     }
 }
