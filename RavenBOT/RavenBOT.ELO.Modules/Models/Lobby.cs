487fda04c8630a2b1cb9c88babeae30cdc128356
PassiveModding <passivemodding@gmail.com>
2019-07-30 15:38:12 +1000

    Inherit from ELOLobby for lobbies
    this is to potentially help add support for different lobby types in future

--- RavenBOT/RavenBOT.ELO.Modules/Models/Lobby.cs
+++ RavenBOT/RavenBOT.ELO.Modules/Models/Lobby.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
+using RavenBOT.ELO.Modules.Modules;
 
 namespace RavenBOT.ELO.Modules.Models
 {
-    public class Lobby
+    public class Lobby : EloLobby
     {
         public static string DocumentName(ulong guildId, ulong channelId)
         {
@@ -14,13 +15,13 @@ namespace RavenBOT.ELO.Modules.Models
             this.GuildId = guildId;
             this.ChannelId = channelId;
         }
+
         public ulong GuildId { get; set; }
         public ulong ChannelId { get; set; }
 
         public int? MinimumPoints { get; set; } = null;
 
         public int PlayersPerTeam { get; set; } = 5;
-
 
         public List<ulong> Queue { get; set; } = new List<ulong>();
 
@@ -29,7 +30,6 @@ namespace RavenBOT.ELO.Modules.Models
         public int CurrentGameCount { get; set; } = 0;
 
         //TODO: Specific announcement channel per lobby
-
         public enum PickMode
         {
             Captains_HighestRanked,
@@ -41,6 +41,8 @@ namespace RavenBOT.ELO.Modules.Models
 
         //TODO: Allow for votes on maps, reduce change of repeate games on the same map.
         public List<string> Maps { get; set; } = new List<string>();
+
+        //TODO: Instead of storing map history here use the maps that were chosen for recent games?
         public List<string> MapHistory { get; set; } = new List<string>();
     }
 }
