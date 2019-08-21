637784977c48147791429890b42998f995014c3c
PassiveModding <passivemodding@gmail.com>
2019-08-21 23:08:35 +1000

    Added additional items to log context and fixed incorrect guildId logging

--- RavenBOT/RavenBOT.Common/Models/LogContext.cs
+++ RavenBOT/RavenBOT.Common/Models/LogContext.cs
@@ -5,15 +5,21 @@ namespace RavenBOT.Common
     public class LogContext
     {
         public ulong userId { get; set; }
+        public string userName { get; set; }
         public ulong guildId { get; set; }
+        public string guildName { get; set; }
         public ulong channelId { get; set; }
+        public string channelName { get; set; }
         public string message { get; set; }
 
         public LogContext(ICommandContext context)
         {
             userId = context.User?.Id ?? 0;
-            guildId = context.Channel?.Id ?? 0;
+            userName = context.User?.Username;
+            guildId = context.Guild?.Id ?? 0;
+            guildName = context.Guild?.Name;
             channelId = context.Channel?.Id ?? 0;
+            channelName = context.Channel?.Name;
             message = context.Message?.Content;
         }
     }
