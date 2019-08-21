d8f589081bf0f26b5b37b93326bf84075746a204
PassiveModding <passivemodding@gmail.com>
2019-08-21 21:50:31 +1000

    Added commands to modify wins, draws and losses directly

--- RavenBOT/RavenBOT.ELO.Modules/Modules/PlayerManagement.cs
+++ RavenBOT/RavenBOT.ELO.Modules/Modules/PlayerManagement.cs
@@ -50,6 +50,69 @@ namespace RavenBOT.ELO.Modules.Modules
             Service.Database.StoreMany(players, x => Player.DocumentName(x.GuildId, x.UserId));
             await ReplyAsync("", false, responseString.QuickEmbed());
         }
+        
+        [Command("Wins")]
+        public async Task WinsAsync(SocketGuildUser user, Player.ModifyState state, int amount)
+        {
+            await WinsAsync(state, amount, user);
+        }
+
+        [Command("Wins")]
+        public async Task WinsAsync(Player.ModifyState state, int amount, params SocketGuildUser[] users)
+        {
+            var players = users.Select(x => Service.GetPlayer(Context.Guild.Id, x.Id)).ToList();
+            var responseString = "";
+            foreach (var player in players)
+            {
+                var newVal = Player.ModifyValue(state, player.Wins, amount);
+                responseString += $"{player.DisplayName}: {player.Wins} => {newVal}\n";
+                player.Wins = newVal;
+            }
+            Service.Database.StoreMany(players, x => Player.DocumentName(x.GuildId, x.UserId));
+            await ReplyAsync("", false, responseString.QuickEmbed());
+        }
+
+        [Command("Losses")]
+        public async Task LossesAsync(SocketGuildUser user, Player.ModifyState state, int amount)
+        {
+            await LossesAsync(state, amount, user);
+        }
+
+        [Command("Losses")]
+        public async Task LossesAsync(Player.ModifyState state, int amount, params SocketGuildUser[] users)
+        {
+            var players = users.Select(x => Service.GetPlayer(Context.Guild.Id, x.Id)).ToList();
+            var responseString = "";
+            foreach (var player in players)
+            {
+                var newVal = Player.ModifyValue(state, player.Losses, amount);
+                responseString += $"{player.DisplayName}: {player.Losses} => {newVal}\n";
+                player.Losses = newVal;
+            }
+            Service.Database.StoreMany(players, x => Player.DocumentName(x.GuildId, x.UserId));
+            await ReplyAsync("", false, responseString.QuickEmbed());
+        }
+
+        [Command("Draws")]
+        public async Task DrawsAsync(SocketGuildUser user, Player.ModifyState state, int amount)
+        {
+            await DrawsAsync(state, amount, user);
+        }
+
+        [Command("Draws")]
+        public async Task DrawsAsync(Player.ModifyState state, int amount, params SocketGuildUser[] users)
+        {
+            var players = users.Select(x => Service.GetPlayer(Context.Guild.Id, x.Id)).ToList();
+            var responseString = "";
+            foreach (var player in players)
+            {
+                var newVal = Player.ModifyValue(state, player.Draws, amount);
+                responseString += $"{player.DisplayName}: {player.Draws} => {newVal}\n";
+                player.Draws = newVal;
+            }
+            Service.Database.StoreMany(players, x => Player.DocumentName(x.GuildId, x.UserId));
+            await ReplyAsync("", false, responseString.QuickEmbed());
+        }
 
         [Command("PlayerModify")]
         public async Task PlayerModifyAsync(SocketGuildUser user, string value, Player.ModifyState state, int amount)
