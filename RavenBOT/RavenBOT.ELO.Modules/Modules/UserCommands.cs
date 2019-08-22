9d6ffbf09451fd8061fe6f9adfe23dba38bd31c9
PassiveModding <passivemodding@gmail.com>
2019-08-22 23:41:04 +1000

    Moved register and rename command to UserCommands module

---
+++ RavenBOT/RavenBOT.ELO.Modules/Modules/UserCommands.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Threading.Tasks;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using RavenBOT.Common;
+using RavenBOT.ELO.Modules.Methods;
+
+namespace RavenBOT.ELO.Modules.Modules
+{
+    public class UserCommands : InteractiveBase<ShardedCommandContext>
+    {
+        public ELOService Service { get; }
+
+        public UserCommands(ELOService service)
+        {
+            Service = service;
+        }
+
+        [Command("Register", RunMode = RunMode.Sync)]
+        public async Task RegisterAsync([Remainder]string name = null)
+        {
+            if (name == null)
+            {
+                name = (Context.User as SocketGuildUser)?.Nickname ?? Context.User.Username;
+            }
+
+            //TODO: Add option to prevent re-registering
+            //TODO: Add precondition for premium  
+            //TODO: Fix name not being set when re-registering
+
+            var player = Service.GetPlayer(Context.Guild.Id, Context.User.Id) ?? Service.CreatePlayer(Context.Guild.Id, Context.User.Id, name);
+            var competition = Service.GetOrCreateCompetition(Context.Guild.Id);
+            var responses = await Service.UpdateUserAsync(competition, player, Context.User as SocketGuildUser);
+            await ReplyAsync($"You have registered as `{name}`, all roles/name updates have been applied if applicable.");
+            if (responses.Count > 0)
+            {
+                await ReplyAndDeleteAsync("", false, String.Join("\n", responses).QuickEmbed(), TimeSpan.FromSeconds(30));
+            }
+        }
+
+        [Command("Rename", RunMode = RunMode.Sync)]
+        public async Task RenameAsync([Remainder]string name)
+        {
+            var player = Service.GetPlayer(Context.Guild.Id, Context.User.Id);
+            if (player == null)
+            {
+                await ReplyAsync("You are not registered yet.");
+                return;
+            }
+
+            var competition = Service.GetOrCreateCompetition(Context.Guild.Id);
+
+            var newName = competition.GetNickname(player);
+            var gUser = (Context.User as SocketGuildUser);
+            var currentName = gUser.Nickname ?? gUser.Username;
+            if (!currentName.Equals(newName))
+            {
+                if (gUser.Hierarchy < Context.Guild.CurrentUser.Hierarchy)
+                {
+                    if (Context.Guild.CurrentUser.GuildPermissions.ManageNicknames)
+                    {
+                        await gUser.ModifyAsync(x => x.Nickname = newName);
+                    }
+                    else
+                    {
+                        await ReplyAsync("The bot does not have the `ManageNicknames` permission and therefore cannot update your nickname.");
+                    }
+                }
+                else
+                {
+                    await ReplyAsync("You have a higher permission level than the bot and therefore it cannot update your nickname.");
+                }
+            }
+
+            player.DisplayName = name;
+            Service.SavePlayer(player);
+            await ReplyAsync($"Your profile has been renamed from {player.DisplayName} to {name}");
+        }
+    }
+}
