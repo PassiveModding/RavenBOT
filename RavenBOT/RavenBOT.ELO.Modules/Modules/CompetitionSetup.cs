e85ed8964109347e9b3e7da5f798662603eaa739
PassiveModding <passivemodding@gmail.com>
2019-08-26 10:46:05 +1000

    Update register role method to show more useful responses including querying info

--- RavenBOT/RavenBOT.ELO.Modules/Modules/CompetitionSetup.cs
+++ RavenBOT/RavenBOT.ELO.Modules/Modules/CompetitionSetup.cs
@@ -14,25 +14,55 @@ namespace RavenBOT.ELO.Modules.Modules
     public class CompetitionSetup : InteractiveBase<ShardedCommandContext>
     {
         public ELOService Service { get; }
+        public PrefixService Prefix { get; }
 
-        public CompetitionSetup(ELOService service)
+        public CompetitionSetup(ELOService service, PrefixService prefix)
         {
+            this.Prefix = prefix;
             Service = service;
         }
 
         [Command("SetRegisterRole", RunMode = RunMode.Sync)]
-        [Alias("Set RegisterRole")]
-        public async Task SetRegisterRole(IRole role)
+        [Alias("Set RegisterRole", "RegisterRole")]
+        [Summary("Sets or displays the current register role")]
+        public async Task SetRegisterRole([Remainder] IRole role = null)
         {
             var competition = Service.GetOrCreateCompetition(Context.Guild.Id);
+            if (role == null)
+            {
+                if (competition.RegisteredRankId != 0)
+                {
+                    var gRole = Context.Guild.GetRole(competition.RegisteredRankId);
+                    if (gRole == null)
+                    {
+                        //Rank previously set but can no longer be found (deleted)
+                        //May as well reset it.
+                        competition.RegisteredRankId = 0;
+                        Service.SaveCompetition(competition);
+                        await ReplyAsync("Register role had previously been set but can no longer be found in the server. It has been reset.");
+                    }
+                    else
+                    {
+                        await ReplyAsync($"Current register role is: {gRole.Mention}");
+                    }
+                }
+                else
+                {
+                    var serverPrefix = Prefix.GetPrefix(Context.Guild.Id) ?? Prefix.DefaultPrefix;
+                    await ReplyAsync($"There is no register role set. You can set one with `{serverPrefix}SetRegisterRole @role` or `{serverPrefix}SetRegisterRole rolename`");
+                }
+
+                return;
+            }
+
             competition.RegisteredRankId = role.Id;
             Service.SaveCompetition(competition);
-            await ReplyAsync("Register role set.");
+            await ReplyAsync($"Register role set to {role.Mention}");
         }
 
         [Command("SetNicknameFormat", RunMode = RunMode.Sync)]
-        [Alias("Set NicknameFormat", "NameFormat", "SetNameFormat")]
-        public async Task SetNicknameFormatAsync([Remainder]string format)
+        [Alias("Set NicknameFormat", "NicknameFormat", "NameFormat", "SetNameFormat")]
+        public async Task SetNicknameFormatAsync([Remainder] string format)
         {
             var competition = Service.GetOrCreateCompetition(Context.Guild.Id);
             competition.NameFormat = format;
@@ -42,7 +72,7 @@ namespace RavenBOT.ELO.Modules.Modules
             testProfile.Draws = 1;
             testProfile.Points = 600;
             var exampleNick = competition.GetNickname(testProfile);
-            
+
             Service.SaveCompetition(competition);
             await ReplyAsync($"Nickname Format set.\nExample: `{exampleNick}`");
         }
@@ -52,16 +82,16 @@ namespace RavenBOT.ELO.Modules.Modules
         public async Task ShowNicknameFormatsAsync()
         {
             var response = "**NickNameFormats**\n" + // Use Title
-                           "{score} - Total points\n" +
-                           "{name} - Registration name\n" +
-                           "{wins} - Total wins\n" +
-                           "{draws} - Total draws\n" +
-                           "{losses} - Total losses\n" +
-                           "{games} - Games played\n\n" +
-                           "Examples:\n" +
-                           "`SetNicknameFormat {score} - {name}` `1000 - Player`\n" +
-                           "`SetNicknameFormat [{wins}] {name}` `[5] Player`\n" +
-                           "NOTE: Nicknames are limited to 32 characters long on discord";
+                "{score} - Total points\n" +
+                "{name} - Registration name\n" +
+                "{wins} - Total wins\n" +
+                "{draws} - Total draws\n" +
+                "{losses} - Total losses\n" +
+                "{games} - Games played\n\n" +
+                "Examples:\n" +
+                "`SetNicknameFormat {score} - {name}` `1000 - Player`\n" +
+                "`SetNicknameFormat [{wins}] {name}` `[5] Player`\n" +
+                "NOTE: Nicknames are limited to 32 characters long on discord";
 
             await ReplyAsync("", false, response.QuickEmbed());
         }
